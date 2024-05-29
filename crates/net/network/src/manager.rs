f62ec4bab6cbe96c11ba12449e27d237be39eb47
Keefe Liu <keefe.l@users.noreply.github.com>
2024-05-29 11:37:25 +0800

    comment disconnect

--- crates/net/network/src/manager.rs
+++ crates/net/network/src/manager.rs
@@ -489,17 +489,17 @@ where
     fn on_peer_message(&mut self, peer_id: PeerId, msg: PeerMessage) {
         match msg {
             PeerMessage::NewBlockHashes(hashes) => {
-                self.within_pow_or_disconnect(peer_id, |this| {
-                    // update peer's state, to track what blocks this peer has seen
-                    this.swarm.state_mut().on_new_block_hashes(peer_id, hashes.0)
-                })
+                // self.within_pow_or_disconnect(peer_id, |this| {
+                //     // update peer's state, to track what blocks this peer has seen
+                //     this.swarm.state_mut().on_new_block_hashes(peer_id, hashes.0)
+                // })
             }
             PeerMessage::NewBlock(block) => {
-                self.within_pow_or_disconnect(peer_id, move |this| {
-                    this.swarm.state_mut().on_new_block(peer_id, block.hash);
-                    // start block import process
-                    this.block_import.on_new_block(peer_id, block);
-                });
+                // self.within_pow_or_disconnect(peer_id, move |this| {
+                //     this.swarm.state_mut().on_new_block(peer_id, block.hash);
+                //     // start block import process
+                //     this.block_import.on_new_block(peer_id, block);
+                // });
             }
             PeerMessage::PooledTransactions(msg) => {
                 self.notify_tx_manager(NetworkTransactionEvent::IncomingPooledTransactionHashes {
